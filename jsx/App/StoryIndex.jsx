4307fcee691794d1aaaca18544e9edafc37d6cbe
gelcop <grupo.estudiolenguas@pucp.edu.pe>
2026-01-24 15:59:40 -0500

    Update StoryIndex to show all stories by default
    Modified StoryIndex.jsx so that all stories are displayed by default when the page loads, instead of requiring a search. This improves usability by making the full story list immediately visible.

--- jsx/App/StoryIndex.jsx
+++ jsx/App/StoryIndex.jsx
@@ -1,71 +1,71 @@
 import React from 'react';
-// import id from 'shortid';
-// import { Link } from 'react-router-dom';
-// import ReactDOMServer from 'react-dom/server';
 import { TranslatableText } from './locale/TranslatableText.jsx';
-import { indexPageTitleHeaderText, indexPageAuthorHeaderText, indexPageMediaHeaderText } from './locale/LocaleConstants.jsx';
+import { 
+  indexPageTitleHeaderText, 
+  indexPageMediaHeaderText 
+} from './locale/LocaleConstants.jsx';
 
 require('datatables.net-dt');
 import 'datatables.net-dt/css/jquery.dataTables.css';
 
 export class StoryIndex extends React.Component {
+  async componentDidMount() {
+    const index = (await import('~./data/index.json')).default;
+    let storyList = [];
 
-    async componentDidMount() {
-        const index = (await import('~./data/index.json')).default;
-        let storyList = [];
-        for (const story in index) {
-            if (index.hasOwnProperty(story)) {
-                /////////
-                // Title
-                /////////
-                let mainTitle = index[story]['title']['con-Latn-EC'];
-                if (index[story]['title']['_default'] != '') {
-                    mainTitle = index[story]['title']['_default'];
-                }
-
-                let timed = '';
-                if (index[story]['timed']) {
-                    if (index[story]['media']['audio'] != '') {
-                        timed += '🎧 &nbsp;&nbsp;'
-                    }
-                    if (index[story]['media']['video'] != '') {
-                        timed += '🎞 &nbsp;&nbsp;'
-                    }
-                } else {
-                    timed = '✘';
-                }
-                // React Router link:
-                const link = `<a href='#/story/${index[story]['story ID']}'>${mainTitle}</a>`;
-
-                storyList.push([link, index[story]['author'], timed]);
-            }
+    for (const story in index) {
+      if (index.hasOwnProperty(story)) {
+        // Title + Subtitle
+        let mainTitle = index[story]['title']['con-Latn-EC'];
+        if (index[story]['title']['_default'] !== '') {
+          mainTitle = index[story]['title']['_default'];
+        }
+        const subtitle = index[story]['subtitle'] || '';
+        let titleWithSubtitle = mainTitle;
+        if (subtitle !== '') {
+          titleWithSubtitle += `<br/><span class="indexSubtitle">${subtitle}</span>`;
         }
 
-        $(document).ready(function() {
-            $('#indexTable').DataTable( {
-                data: storyList,
-                columns: [ {}, {}, {} ],
-                scrollY: '75vh',
-                scrollCollapse: true,
-                paging: false
-            });
-        });
-        $('#indexTable').addClass("stripe");
+        // React Router link, wrapping title + subtitle
+        const link = `<a href='#/story/${index[story]['story ID']}'>${titleWithSubtitle}</a>`;
+
+        const narration = index[story]['narration'] || '';
+        const translation = index[story]['translation'] || '';
+        const language = index[story]['author'] || '';
+        storyList.push([link, language, narration, translation]);
+      }
     }
 
-    render() {
-        return (
-            <div id="index">
-                <table id="indexTable">
-                  <thead>
-                    <tr>
-                        <th><TranslatableText dictionary={indexPageTitleHeaderText} /></th>
-                        <th><TranslatableText dictionary={indexPageAuthorHeaderText} /></th>
-                        <th><TranslatableText dictionary={indexPageMediaHeaderText} /></th>
-                    </tr>
-                  </thead>
-                </table>
-            </div>
-        );
-    }
+    $(document).ready(function () {
+      $('#indexTable').DataTable({
+        data: storyList,
+        columns: [{}, {}, {}, {}], 
+        scrollY: '75vh',
+        scrollCollapse: true,
+        paging: false,
+        columnDefs: [
+          { className: "dt-center", targets: "_all" } // 👈 centrado global
+        ]
+      });
+    });
+
+    $('#indexTable').addClass("stripe");
+  }
+
+  render() {
+    return (
+      <div id="index">
+        <table id="indexTable">
+          <thead>
+            <tr>
+              <th>Título</th>
+              <th>Lengua</th>
+              <th>Narración</th>
+              <th>Traducción</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    );
+  }
 }
